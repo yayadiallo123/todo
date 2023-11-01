d967a392d3d82de068a19cfe967541d74750c6f5
yaya <yeleta2014yaya@gmail.com>
2023-11-01 14:31:06 +0000

    feature edit and delete

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,13 @@ from django.urls import path
 from  . import views
 urlpatterns = [
     path('addTask/', views.addTask,name='addTask'),
+    path('mark_as_done/<int:pk>',views.mark_as_done,name='mark_as_done'),
+    # Mark as undone
+    path('mark_us_done/<int:pk>',views.mark_us_done,name='mark_us_done'),
+    
+    # Edit feaure 
+    path('edit_task/<int:pk>',views.edit_task, name='edit_task'),
+    
+     # Delete feaure 
+    path('delete_task/<int:pk>',views.delete_task, name='delete_task'),
 ]
